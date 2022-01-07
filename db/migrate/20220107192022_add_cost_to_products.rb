fb99d4a89f159a11eb26a653a1e75f5d8b8c5bf2
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-07 11:25:18 -0800

    change product.cost column from int to float

---
+++ db/migrate/20220107192022_add_cost_to_products.rb
@@ -0,0 +1,6 @@
+class AddCostToProducts < ActiveRecord::Migration[5.2]
+  def change
+    remove_column(:products, :cost)
+    add_column(:products, :cost, :float)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_07_164804) do
+ActiveRecord::Schema.define(version: 2022_01_07_192022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "cost"
     t.string "country_of_origin"
     t.boolean "featured"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "cost"
   end
 
   create_table "reviews", force: :cascade do |t|
