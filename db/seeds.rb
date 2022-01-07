930054ff1552ec6822a7fc8ded1c330d791f664d
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-07 12:08:08 -0800

    add database seeds with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+20.times do |index|
+  product = Product.create!({name: Faker::Food.unique.spice,
+    country_of_origin: Faker::Books::Dune.city,
+    cost: rand(3..99)})
+  rand(3..11).times do |index|
+    Review.create!({author: Faker::Books::Dune.character, 
+      content_body: Faker::Books::Dune.quote,
+      rating: rand(1..5),
+      product_id: product.id})
+  end
+end
+
+p "Created #{Product.count} products"
+p "Created #{Review.count} products"
