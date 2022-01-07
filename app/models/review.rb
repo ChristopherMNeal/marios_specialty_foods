d1a463a212bef3142a426dfb748183672b0a2351
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-07 13:02:41 -0800

    pass review content length test

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
   validates :author, presence: true
   validates :content_body, presence: true
   validates :rating, presence: true
-
+  validates_length_of :content_body, minimum: 50, maximum: 250
 end
