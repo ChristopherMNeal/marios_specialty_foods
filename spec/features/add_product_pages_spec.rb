0c0eedf9f835f6bf964e71bf4c4b484c92aa2b9d
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-08 10:18:41 -0800

    add integration tests for add product

---
+++ spec/features/add_product_pages_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "the add a product process" do
+  it "adds a new product" do
+    visit root_path
+    click_link 'Add new Spice'
+    fill_in 'Product', :with => 'the essence'
+    fill_in 'Planet of Origin', :with => 'Arrakis'
+    fill_in 'Cost in Imperial Solari ☼', :with => '998'
+    click_on 'Create Product'
+    expect(page).to have_content 'Spice successfully added!'
+    expect(page).to have_content 'The Essence (Arrakis) - ☼998.0'
+  end
+
+  it "gives an error when no name is entered" do
+    visit new_product_path
+    click_on 'Create Product'
+    expect(page).to have_content "Name can't be blank"
+  end
+end
