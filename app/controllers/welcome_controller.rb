41d0db1bd37296825d097386bd2250e42ac6fa06
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-07 16:14:34 -0800

    add instance variables to welcome controller for scope

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    @products = Product.all
+    @reviews = Reviews.all
+    @products_most_recent = Product.most_recent
+    @products_most_reviews = Product.most_reviews
   end
 end
