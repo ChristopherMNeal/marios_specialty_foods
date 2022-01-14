549557a3d725dc45bcdf68580b846260970b6700
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-14 10:54:42 -0800

    add users to database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,25 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.destroy_all
+User.destroy_all
+
+1.times do |index|
+  user = User.create!({email: 'admin@fake.com', password: 'testing', admin: true})
+end
+1.times do |index|
+  user = User.create!({email: 'user@fake.com', password: 'testing', admin: false})
+end
 
 melange = Product.create({ name: "Melange", country_of_origin: "Dune", cost: 998})
 Review.create({ author: "Muad'Dib", content_body: "Bless the Maker and His water. Bless the coming and going of Him. May His passage cleanse the world. May He keep the world for His people.", rating: 5, product_id: melange.id})
+
 49.times do |index|
   product = Product.create!({name: Faker::Food.spice,
     country_of_origin: Faker::Books::Dune.planet,
     cost: rand(3..99)})
-  rand(3..11).times do |index|
-    Review.create!({author: Faker::Books::Dune.character, 
-      content_body: Faker::Lorem.paragraph_by_chars(number: 200),
-      # content_body: Faker::Books::Dune.quote
+  rand(4..12).times do |index|
+    Review.create({author: Faker::Books::Dune.character,
+      content_body: Faker::Books::Dune.quote,
       rating: rand(1..5),
       product_id: product.id})
   end
