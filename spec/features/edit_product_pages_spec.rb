94f5ac9fbcd16bf2ba4038ace6e21cf13cf14cfd
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-14 12:09:07 -0800

    update and pass authroization test to edit product

--- spec/features/edit_product_pages_spec.rb
+++ spec/features/edit_product_pages_spec.rb
@@ -21,6 +21,18 @@ describe "the edit a product process" do
     fill_in 'Product', :with => ''
     click_on 'Update Product'
     expect(page).to have_content 'There was an error in updating your Spice!'
+  end
+end
 
+describe "the unauthorized to edit a product if not admin process" do
+  before :each do
+    user = User.create!({email: 'user2@fake.com', password: 'f4k3p455w0rd', admin: false})
+    login_as(user, :scope => :user)
+    @product = Product.create!({name: 'the essence', country_of_origin: 'Arrakis', cost: '998'})
+  end
+  
+  it "fails to edits a new product" do
+    visit edit_product_path(@product)
+    expect(page).to have_content 'You are not authorized for this action.'
   end
 end
