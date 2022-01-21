4d6ff1a74b46f7b563b611a84fd9006e3d7042be
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-20 20:24:39 -0800

    add and pass 'fail to delete product' test

--- spec/features/delete_product_pages_spec.rb
+++ spec/features/delete_product_pages_spec.rb
@@ -19,10 +19,11 @@ describe "the unauthorized to delete a product if not admin process" do
     user = User.create!({email: 'user2@fake.com', password: 'f4k3p455w0rd', admin: false})
     login_as(user, :scope => :user)
     @product = Product.create!({name: 'the essence', country_of_origin: 'Arrakis', cost: '998'})
+    visit products_path
   end
   
   it "fails to delete a product" do
-    # page.driver.delete("/product/#{@product.id}")
-    # expect(page).to have_content 'You are not authorized for this action.'
+    click_link 'The Essence (Arrakis) - ☼998.0'
+    expect(page).to have_no_content 'Delete spice'
   end
 end
