7a254d5d4bd1dc6a61d3a4f77e8104c30f6d7b8d
Christopher Neal <christopher.m.neal@gmail.com>
2022-01-20 20:24:21 -0800

    add and pass 'fail to delete review' test

--- spec/features/delete_review_pages_spec.rb
+++ spec/features/delete_review_pages_spec.rb
@@ -16,3 +16,19 @@ describe "the delete a review process" do
     expect(page).to have_no_content "Muad'dib - 5 stars"
   end
 end
+
+describe "the delete a review process" do
+  before :each do
+    user = User.create!({email: 'user2@fake.com', password: 'f4k3p455w0rd', admin: false})
+    login_as(user, :scope => :user)
+    product = Product.create!({name: 'the essence', country_of_origin: 'Arrakis', cost: '998'})
+    Review.create!({author: "Muad'dib", content_body: "Bless the Maker and His water. Bless the coming and going of Him. May His passage cleanse the world. May He keep the world for His people.", rating: 5, product_id: product.id})
+    visit products_path
+  end
+  
+  it "deletes a review" do
+    click_link 'The Essence (Arrakis) - ☼998.00'
+    click_link "Muad'dib - 5 stars"
+    expect(page).to have_no_content 'Delete review'
+  end
+end
